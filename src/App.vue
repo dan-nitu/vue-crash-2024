1cc224eb32967ce4db7a2e2b8d5d0c5cc28e1221
Voodoo30fps <voodoo30fps@gmail.com>
2025-02-20 12:27:41 +0200

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ export default {
       name: 'John Doe',
       status: 'active',
       tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://google.com',
     };
   },
 };
@@ -20,4 +21,7 @@ export default {
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
+
+  <a v-bind:href="link">Click for Google</a>
+  <a :href="link">Click for Google</a>
 </template>
