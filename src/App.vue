024be3d879488e2dcfd85f9d9b7c00b10fd78d90
Voodoo30fps <voodoo30fps@gmail.com>
2025-02-23 20:05:51 +0200

    hero section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Navbar from './assets/components/Navbar.vue';
+import Hero from './assets/components/Hero.vue';
 </script>
 
 <template>
   <Navbar />
+  <Hero />
 </template>
