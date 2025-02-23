ba9c46fd88d76ae50963c7f80f1210b40a630c9f
Voodoo30fps <voodoo30fps@gmail.com>
2025-02-23 20:11:37 +0200

    homecards

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Navbar from './assets/components/Navbar.vue';
 import Hero from './assets/components/Hero.vue';
+
+import HomeCards from './assets/components/HomeCards.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
+
+  <HomeCards />
 </template>
